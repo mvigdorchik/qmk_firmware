ef49685a53d56d30af7b53a6f93d484f445f3dcf
Max Vigdorchik <max@vigdorchik.org>
2020-05-07 21:39:23 -0400

    Minor changes to add a right alt function

--- keyboards/preonic/keymaps/MaxVigdorchik/keymap.c
+++ keyboards/preonic/keymaps/MaxVigdorchik/keymap.c
@@ -51,25 +51,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	 */
 	[_QWERTY] = LAYOUT_preonic_grid(KC_GRV,			KC_1,	 KC_2,	  KC_3,	   KC_4,  KC_5,	  KC_6,	  KC_7,	 KC_8,	  KC_9,	   KC_0,	KC_BSPC,
 					KC_TAB,			KC_Q,	 KC_W,	  KC_E,	   KC_R,  KC_T,	  KC_Y,	  KC_U,	 KC_I,	  KC_O,	   KC_P,	KC_BSLS,
-					RCTL_T(KC_ESC), KC_A,	 KC_S,	  KC_D,	   KC_F,  KC_G,	  KC_H,	  KC_J,	 KC_K,	  KC_L,	   KC_SCLN, KC_QUOT,
+					RCTL_T(KC_ESC), KC_A,	 KC_S,	  KC_D,	   KC_F,  KC_G,	  KC_H,	  KC_J,	 KC_K,	  KC_L,	   KC_SCLN, RALT_T(KC_QUOT),
 					KC_LSFT,		KC_Z,	 KC_X,	  KC_C,	   KC_V,  KC_B,	  KC_N,	  KC_M,	 KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
 					KC_LCTL,		KC_LCTL, KC_LGUI, KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,	KC_RGHT),
 
 	/* Lower
 	 */
-	[_LOWER] = LAYOUT_preonic_grid(KC_F11,	KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,	 KC_NLCK, KC_P7,   KC_P8,	KC_P9,	 KC_PMNS, KC_BSPC,
-				   KC_F12,	KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,	 KC_INS,  KC_P4,   KC_P5,	KC_P6,	 KC_PPLS, KC_DEL,
-				   KC_CAPS, MEH(KC_F1), MEH(KC_F2), MEH(KC_F3), MEH(KC_F4), KC_BSLS, KC_PSCR, KC_P1,   KC_P2,	KC_P3,	 KC_PAST, KC_PEQL,
-				   _______, KC_LBRC,	KC_RBRC,	KC_MINS,	KC_EQL,		KC_SLSH, KC_PAUS, KC_P0,   KC_PDOT, KC_PCMM, KC_PSLS, _______,
-				   _______, _______,	_______,	_______,	_______,	_______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END),
+	[_LOWER] = LAYOUT_preonic_grid(
+		KC_F11,	KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,	 KC_NLCK, KC_P7,   KC_P8,	KC_P9,	 KC_PMNS, KC_BSPC,
+		KC_F12,	KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,	 KC_INS,  KC_P4,   KC_P5,	KC_P6,	 KC_PPLS, KC_DEL,
+		KC_CAPS, MEH(KC_F1), MEH(KC_F2), MEH(KC_F3), MEH(KC_F4), KC_BSLS, KC_PSCR, KC_P1,   KC_P2,	KC_P3,	 KC_PAST, KC_PEQL,
+		_______, KC_LBRC,	KC_RBRC,	KC_MINS,	KC_EQL,		KC_SLSH, KC_PAUS, KC_P0,   KC_PDOT, KC_PCMM, KC_PSLS, RSFT_T(KC_PENT),
+		_______, _______,	_______,	_______,	_______,	_______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END),
 
 	/* Raise
+	 */ // TODO: Move Math mode to the adjust layer so it is harder to accidently press
+	[_RAISE] = LAYOUT_preonic_grid(
+		KC_F11,	KC_F1,	 KC_F2,	  KC_F3,   KC_F4,	KC_F5,	 KC_F6,		   KC_F7,	KC_F8,	 KC_F9,	  KC_F10,  KC_BSPC,
+		KC_F12,	KC_LPRN, KC_RPRN, KC_MINS, KC_UNDS, KC_BSLS, KC_PIPE,	   KC_HOME, KC_UP,	 KC_END,  XXXXXXX, KC_DEL,
+		_______, KC_LBRC, KC_RBRC, KC_EQL,  KC_PLUS, KC_GRV,	 LSFT(KC_GRV), KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, KC_LOCK,
+		_______, KC_LCBR, KC_RCBR, KC_LT,   KC_GT,	KC_SCLN, KC_COLN,	   KC_MUTE, KC_MUTE, KC_MPLY, KC_VOLU, _______,
+		_______, _______, _______, _______, _______, _______, _______,	   _______, KC_APP,	 KC_MPRV, KC_VOLD, KC_MNXT),
+
+	/* Adjust (Lower + Raise)
 	 */
-	[_RAISE] = LAYOUT_preonic_grid(KC_F11,	KC_F1,	 KC_F2,	  KC_F3,   KC_F4,	KC_F5,	 KC_F6,		   KC_F7,	KC_F8,	 KC_F9,	  KC_F10,  KC_BSPC,
-				   KC_F12,	KC_LPRN, KC_RPRN, KC_MINS, KC_UNDS, KC_BSLS, KC_PIPE,	   KC_HOME, KC_UP,	 KC_END,  XXXXXXX, KC_DEL,
-				   _______, KC_LBRC, KC_RBRC, KC_EQL,  KC_PLUS, KC_GRV,	 LSFT(KC_GRV), KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, KC_LOCK,
-				   _______, KC_LCBR, KC_RCBR, KC_LT,   KC_GT,	KC_SCLN, KC_COLN,	   TG(_MATH), KC_MUTE, KC_MPLY, KC_VOLU, _______,
-				   _______, _______, _______, _______, _______, _______, _______,	   _______, KC_APP,	 KC_MPRV, KC_VOLD, KC_MNXT),
+	[_ADJUST] = LAYOUT_preonic_grid(
+		KC_VOLD, KC_VOLU, KC_BRID, KC_BRIU, KC_PSCR, KC_SLCK,  KC_PAUS,  KC_INS, _______,  DM_REC1,  DM_REC2,  DM_RSTP,
+		_______, RESET,  DEBUG, _______, _______, UNICODE_MODE_OSX,  _______,  _______, _______,  DM_PLY1,  DM_PLY2,  KC_DEL,
+		_______, _______, MU_MOD, AU_ON, AU_OFF, UNICODE_MODE_WIN,  _______,  _______, DF(_QWERTY),DF(_MOUSE),  PLOVER,  _______,
+		_______, MUV_DE, MUV_IN, MU_ON, MU_OFF,  UNICODE_MODE_LNX, _______, TG(_MATH), _______,  MAGIC_HOST_NKRO, MAGIC_UNHOST_NKRO, _______,
+		_______, _______, _______, _______, _______, _______,  _______,  _______, _______,  _______,  _______,  _______),
 
 	/* Mouse keys in case there is no mouse connected
 	 */
@@ -86,13 +97,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 				  _______,	 XP(ALPHA,ALEPH),XP(SIGMA, C_SIGMA), XP(DELTA, C_DELTA),XP(PHI,C_PHI), XP(GAMMA,C_GAMMA),_______,_______,X(KAPPA),XP(LAMBDA,C_LAMBDA), _______, _______,
 				  _______,	 X(ZETA),  X(CHI),	 XP(XI,C_XI), _______,	X(BETA), X(NU),	 X(MU), _______,_______,_______,_______,
 				  TG(_MATH),_______,   _______,	 _______, _______,	_______, _______,  _______, _______,_______,_______,_______),
-	/* Adjust (Lower + Raise)
-	 */
-	[_ADJUST] = LAYOUT_preonic_grid(KC_VOLD, KC_VOLU, KC_BRID, KC_BRIU, KC_PSCR, KC_SLCK,		   KC_PAUS,			  KC_INS,			_______,	 DM_REC1,		  DM_REC2,			 DM_RSTP,
-					_______, RESET,	  DEBUG,   _______, _______, _______,		   _______,			  _______,			_______,	 DM_PLY1,		  DM_PLY2,			 KC_DEL,
-					_______, _______, MU_MOD,  AU_ON,	AU_OFF,	 _______,		   _______,			  _______,			DF(_QWERTY),DF(_MOUSE),		  PLOVER,			 _______,
-					_______, MUV_DE,  MUV_IN,  MU_ON,	MU_OFF,	 UNICODE_MODE_LNX, UNICODE_MODE_WINC, UNICODE_MODE_OSX, _______,	 MAGIC_HOST_NKRO, MAGIC_UNHOST_NKRO, _______,
-					_______, _______, _______, _______, _______, _______,		   _______,			  _______,			_______,	 _______,		  _______,			 _______),
 
 	/* PLOVER
 	 */
