bd4254d91725918b9ccc1dc28df05105de95eac9
Max Vigdorchik <max@vigdorchik.org>
2021-04-11 13:44:44 -0500

    Move caps lock to adjust layer

--- keyboards/preonic/keymaps/MaxVigdorchik/keymap.c
+++ keyboards/preonic/keymaps/MaxVigdorchik/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_LOWER] = LAYOUT_preonic_grid(
 		KC_F11, 	KC_F1,  		KC_F2,		KC_F3,		KC_F4,		KC_F5,  	 KC_NLCK, KC_P7,   KC_P8,  	KC_P9,  	 KC_PMNS, KC_BSPC,
 		KC_F12, 	KC_F6,  		KC_F7,		KC_F8,		KC_F9,		KC_F10, 	 KC_INS,  KC_P4,   KC_P5,  	KC_P6,  	 KC_PPLS, KC_DEL,
-		KC_CAPS, MEH(KC_F1), MEH(KC_F2),   MEH(KC_F3), MEH(KC_F4), KC_BSLS,      KC_PSCR,     KC_P1,   KC_P2,  	KC_P3,  	 KC_PAST, KC_PEQL,
+		_______, MEH(KC_F1), MEH(KC_F2),   MEH(KC_F3), MEH(KC_F4), KC_BSLS,      KC_PSCR,     KC_P1,   KC_P2,  	KC_P3,  	 KC_PAST, KC_PEQL,
 		_______, KC_LBRC,   	KC_RBRC,  	KC_MINS,  	KC_EQL,   		KC_SLSH, KC_PAUS,     KC_P0,   KC_PCMM, KC_PDOT, KC_PSLS,     RSFT_T(KC_PENT),
 		_______, _______,   	_______,  	_______,  	_______,  	_______,     _______,     _______, KC_HOME, KC_PGDN, KC_PGUP,     KC_END),
 
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_ADJUST] = LAYOUT_preonic_grid(
 		KC_VOLD, KC_VOLU, KC_BRID, KC_BRIU, KC_PSCR, KC_SLCK,          KC_PAUS, KC_INS,            _______,     DM_REC1,         DM_REC2,           DM_RSTP,
 		_______, RESET,   DEBUG,   _______, _______, UNICODE_MODE_OSX, _______, _______,           _______,     DM_PLY1,         DM_PLY2,           KC_DEL,
-		_______, _______, MU_MOD,  AU_ON,   AU_OFF,  UNICODE_MODE_WIN, _______, DF(_NO_TRANSIENT), DF(_QWERTY), DF(_MOUSE),      PLOVER,            _______,
+		KC_CAPS, _______, MU_MOD,  AU_ON,   AU_OFF,  UNICODE_MODE_WIN, _______, DF(_NO_TRANSIENT), DF(_QWERTY), DF(_MOUSE),      PLOVER,            KC_LOCK,
 		_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  UNICODE_MODE_LNX, _______, TG(_MATH),         _______,     MAGIC_HOST_NKRO, MAGIC_UNHOST_NKRO, _______,
 		_______, _______, _______, _______, _______, _______,          _______, _______,           _______,     _______,         _______,           _______),
 
