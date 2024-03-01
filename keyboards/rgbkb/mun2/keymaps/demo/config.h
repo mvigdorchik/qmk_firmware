9799541853bacc631395f7234ab549147c13f94f
Max Vigdorchik <max@vigdorchik.org>
2024-03-01 01:12:58 -0600

    Update keymaps to latest qmk

--- keyboards/rgbkb/mun2/keymaps/demo/config.h
+++ keyboards/rgbkb/mun2/keymaps/demo/config.h
@@ -9,8 +9,6 @@
 
 #pragma once
 
-#define ENCODER_RESOLUTION 2
-
 // No need for the single versions when multi performance isn't a problem =D
 #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
 #undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
@@ -18,30 +16,6 @@
 #undef ENABLE_RGB_MATRIX_SPLASH
 #undef ENABLE_RGB_MATRIX_SOLID_SPLASH
 
-#undef RGBLIGHT_HUE_STEP
-#undef RGBLIGHT_VAL_STEP
-#define RGBLIGHT_HUE_STEP 1
-#define RGBLIGHT_VAL_STEP 4
-
 // 20m timeout (20m * 60s * 1000mil)
 // #define RGB_DISABLE_TIMEOUT 1200000
 #define RGB_DISABLE_WHEN_USB_SUSPENDED
-
-#define SPLIT_WPM_ENABLE
-
-#define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 2
-// 224B per layer right now
-#define DYNAMIC_KEYMAP_LAYER_COUNT 8
-#define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 2047
-
-#define VIA_QMK_RGBLIGHT_ENABLE
-
-#define STM32_ONBOARD_EEPROM_SIZE 2048
-
-/* Encoder Configuration */
-//      Matrix Entries  k36/k35(E1SW>B13), k33/k32, k7/k28
-#define ENCODERS_PAD_A { A1, B10, A13 }
-#define ENCODERS_PAD_B { A2, B11, B14 }
-//      Matrix Entries  k72/k71(E5SW>B0), k69/k68, k43/k64
-#define ENCODERS_PAD_A_RIGHT { A1, C15,  B3 }
-#define ENCODERS_PAD_B_RIGHT { A2, C14, B13 }
--- keyboards/rgbkb/sol3/keymaps/mvigdorchik/keymap.c
+++ keyboards/rgbkb/sol3/keymaps/mvigdorchik/keymap.c
@@ -54,7 +54,8 @@ enum sol_keycodes {
     MENU_DN,
     RGB_RST,
     NEXT_TAB,
-    PREV_TAB
+    PREV_TAB,
+    RST_ROM
 };
 
 #define LOWER    MO(_LOWER)
@@ -69,10 +70,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    CS_CAMEL,                 MENU_BTN,KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    CS_PASCAL,                XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
  RCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    CS_SNAKE,                 MENU_BTN,KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RALT_T(KC_QUOT),
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    CAPSWRD,                  RGB_TOG, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    CW_TOGG,                  RGB_TOG, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
         KC_LCTL,  KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  RAISE,   LOWER,  KC_BSPC, SELWORD, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RCTL,
 
-        KC_LEFT, KC_RIGHT,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     KC_WH_L, KC_WH_R, XXXXXXX, XXXXXXX, MENU_DN, MENU_UP,
         KC_WH_D, KC_WH_U, XXXXXXX, XXXXXXX, XXXXXXX,                                                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
@@ -83,40 +83,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, KC_ENT,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 
-        _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
     ),
 
     [_LOWER] = LAYOUT(
-        KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                   _______, KC_NLCK, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______,
+        KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                   _______, KC_NUM,  KC_P7,   KC_P8,   KC_P9,   KC_PMNS, _______,
         KC_F12,  KC_F6  , KC_F7,   KC_F8  , KC_F9  , KC_F10,  _______,                   _______, KC_INS,  KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_DEL,
 _______,MEH(KC_F1),MEH(KC_F2),MEH(KC_F3),MEH(KC_F4), KC_BSLS, _______,                   _______, KC_PSCR, KC_P1,   KC_P2,   KC_P3,   KC_PAST, KC_PEQL,
         _______, KC_LBRC, KC_RBRC, KC_MINS,	KC_EQL,  KC_SLSH, _______,                   _______, KC_PAUS, KC_P0,   KC_PCMM, KC_PDOT, KC_PSLS, RSFT_T(KC_PENT),
         _______, _______, _______, _______, ADJUST,  _______, ADJUST,  _______, _______, _______, ADJUST,  KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______,
 
-        PREV_TAB,NEXT_TAB,_______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
     ),
 
     [_RAISE] = LAYOUT(
         KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
         KC_F12,  KC_LPRN, KC_RPRN, KC_MINS, KC_UNDS, KC_BSLS, _______,                   _______, KC_PIPE, KC_HOME, KC_UP,   KC_END,  XXXXXXX, KC_DEL,
-        _______, KC_LBRC, KC_RBRC, KC_EQL,  KC_PLUS, KC_SLSH, _______,                   _______, KC_GRV,  KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, KC_LOCK,
+        _______, KC_LBRC, KC_RBRC, KC_EQL,  KC_PLUS, KC_SLSH, _______,                   _______, KC_GRV,  KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, QK_LOCK,
         _______, KC_LCBR, KC_RCBR, KC_LT  ,	KC_GT ,  KC_SCLN, _______,                   _______, KC_COLN, KC_MUTE, KC_MPLY, KC_MPLY, KC_MPLY, _______,
         _______, _______, _______, _______, ADJUST,  _______, _______, ADJUST,  _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_APP,
 
-        KC_VOLD, KC_VOLU, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
     ),
 
     [_ADJUST] = LAYOUT(
-        KC_F11 , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  DYN_REC_STOP,
-        KC_F12 , RGB_SAD, RGB_VAI, RGB_SAI, QK_BOOT, _______, _______,                   _______, _______, KC_P7,   KC_P8,   KC_P9,   DM_REC1, DM_REC2,
+        KC_F11 , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  DM_RSTP,
+        KC_F12 , RGB_SAD, RGB_VAI, RGB_SAI, QK_BOOT, RST_ROM, _______,                   _______, _______, KC_P7,   KC_P8,   KC_P9,   DM_REC1, DM_REC2,
         KC_CAPS, RGB_HUD, RGB_VAD, RGB_HUI, RGB_RST, _______, _______,                   _______, _______, KC_P4,   KC_P5,   KC_P6,   DM_PLY1, DM_PLY2,
         _______, RGB_SPD, _______, RGB_SPI, _______, _______, _______,                   _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, _______,
-        _______, RGB_RMOD,RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_NLCK, NO_TRNS,  QWERTY,
+        _______, RGB_RMOD,RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_NUM,  NO_TRNS,  QWERTY,
 
-        MENU_DN, MENU_UP, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
     ),
 
@@ -127,11 +123,32 @@ _______,MEH(KC_F1),MEH(KC_F2),MEH(KC_F3),MEH(KC_F4), KC_BSLS, _______,          
         _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 
-        _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______
     ),
 
 };
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [_QWERTY] =        { ENCODER_CCW_CW(KC_RIGHT, KC_LEFT),  ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______),
+                         ENCODER_CCW_CW(KC_WH_R,  KC_WH_L),  ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(MENU_UP, MENU_DN) },
+
+    [_NO_TRANSIENT] =  { ENCODER_CCW_CW(_______, _______),   ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______),
+                         ENCODER_CCW_CW(_______, _______),   ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+
+    [_LOWER] =         { ENCODER_CCW_CW(NEXT_TAB, PREV_TAB), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______),
+                         ENCODER_CCW_CW(_______,  _______),  ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+
+    [_RAISE] =         { ENCODER_CCW_CW(KC_VOLU, KC_VOLD),   ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______),
+                         ENCODER_CCW_CW(_______, _______),   ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+
+    [_ADJUST] =        { ENCODER_CCW_CW(MENU_UP, MENU_DN),   ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______),
+                         ENCODER_CCW_CW(_______, _______),   ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+
+    [_BLANK] =         { ENCODER_CCW_CW(_______, _______),   ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______),
+                         ENCODER_CCW_CW(_______, _______),   ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) },
+};
+#endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_case_modes(keycode, record)) {
@@ -195,9 +212,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING(SS_LCTL(SS_LSFT("\t")));
             }
             return false;
+        case RST_ROM:
+            if (record->event.pressed) {
+                eeconfig_init_kb();
+            }
+            return false;
         default:
             return true;
     }
+}
+
+void keyboard_post_init_user(void)
+{
+    audio_off_user();
 }
 
 void render_layer_status(void) {
